5dd941af480991b7d16c6a4b48985168290d2f05
Federico Gomez Conde <fedeic8@gmail.com>
2023-07-22 10:45:39 -0300

    se agregan funciones para mostrar resultados

--- fizzbuzzpromesas.js
+++ fizzbuzzpromesas.js
@@ -1,11 +1,8 @@
-/* Realiza un el ejercicio de fizzbuzz con promesas.
-Toda validación de cada número debe ser realizado por una función que retorne una promesa.
-Validaciones:
-
-- Sí es múltiplo de 3 debe decir fizz
-- Sí es múltiplo de 5 debe decir buzz
-- Sí es múltiplo de 3 y 5 debe decir fizzbuzz
-- Sí no cumple con ninguna de las condiciones anteriores debe mostrar el número  */
+const ciclos = parseInt(process.argv[2]);
+if (!ciclos) {
+    console.log("Debes introducir un número")
+    process.exit(1)
+  }
 
 const fizzbuzzFunction = (num) => new Promise((resolve, reject) => {
     if (typeof num === 'number'){
@@ -16,6 +13,44 @@ const fizzbuzzFunction = (num) => new Promise((resolve, reject) => {
     }
 })
 
-console.log(fizzbuzzFunction(3));
+function mostrarResultados(ciclos) {
+    const to = (ciclos > 0)? ciclos : 100;
+    const promesas = [];
 
-const result = await fizzbuzzFunction(num);
+    for (let i = 1; i <= to; i++){
+        promesas.push(fizzbuzzFunction(i));
+    }
+    Promise.all(promesas).
+        then((results) => results.forEach((result) => console.log(result)));
+}
+
+/* const randomNumber = (from=1, to=1000) => Math.floor(Math.random() * (to - from + 1)) + from;
+const fizzbuzzFunctionDemora = (num, demora) => new Promise((resolve, reject) => {
+    if (typeof num === 'number'){
+        setTimeout(() => {
+            const result = ((num % 3 ? '' : 'Fizz') + (num % 5 ? '' : 'Buzz')) || num
+            resolve(result);
+        }, demora);
+    } else {
+        reject ('Error: el argumento debe ser un número')
+    }
+})
+
+async function mostrarResultados(ciclos) {
+    const to = (ciclos > 0)? ciclos : 100;
+    // const DEMORA_EN_MS = 0;
+
+    try {
+        for (let i = 1; i <= to; i++){
+            const result = await fizzbuzzFunctionDemora(i, randomNumber());
+            console.log(result);
+            // setTimeout(() => {
+            //     console.log(result);
+            // }, DEMORA_EN_MS * i);
+        }
+    } catch (error) {
+        console.log(error);
+    }    
+} */
+
+mostrarResultados(ciclos);
