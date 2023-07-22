e3a671f74cd5c9c47e267edcf3e76f6fde3ff5e3
Federico Gomez Conde <fedeic8@gmail.com>
2023-07-21 22:05:13 -0300

    se crea funcion que evalua el numero ingresado y devuelve una promesa

--- fizzbuzzpromesas.js
+++ fizzbuzzpromesas.js
@@ -7,17 +7,15 @@ Validaciones:
 - Sí es múltiplo de 3 y 5 debe decir fizzbuzz
 - Sí no cumple con ninguna de las condiciones anteriores debe mostrar el número  */
 
-async function fizzbuzzFunction(num) {
-    //comprobar que 'num' es del tipo number
-    if (typeof num !== 'number') {
-        console.log('Error: el argumento debe ser un número')
+const fizzbuzzFunction = (num) => new Promise((resolve, reject) => {
+    if (typeof num === 'number'){
+        const result = ((num % 3 ? '' : 'Fizz') + (num % 5 ? '' : 'Buzz')) || num
+        resolve(result);
+    } else {
+        reject ('Error: el argumento debe ser un número')
     }
-    //función divisible
+})
 
-    //caso num = 0
-    //caso num divisible por 3 y 5
-    // caso num divisible por 3
-    // caso num divisible por 5
+console.log(fizzbuzzFunction(3));
 
-    //devolver una promesa
-}
+const result = await fizzbuzzFunction(num);
