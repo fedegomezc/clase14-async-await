98bb48abcd5e6e400f79ea5cc61a2a199ee95e93
Federico Gomez Conde <fedeic8@gmail.com>
2023-07-27 11:50:51 -0300

    se modifica parametro alternativo de funcion mostrarResultados

--- fizzbuzzpromesas.js
+++ fizzbuzzpromesas.js
@@ -4,7 +4,7 @@ if (!ciclos) {
     process.exit(1)
   }
 
-const fizzbuzzFunction = (num) => new Promise((resolve, reject) => {
+/* const fizzbuzzFunction = (num) => new Promise((resolve, reject) => {
     if (typeof num === 'number'){
         const result = ((num % 3 ? '' : 'Fizz') + (num % 5 ? '' : 'Buzz')) || num
         resolve(result);
@@ -22,9 +22,10 @@ function mostrarResultados(ciclos) {
     }
     Promise.all(promesas).
         then((results) => results.forEach((result) => console.log(result)));
-}
+} */
 
-/* const randomNumber = (from=1, to=1000) => Math.floor(Math.random() * (to - from + 1)) + from;
+const randomNumber = (from=1, to=1000) => Math.floor(Math.random() * (to - from + 1)) + from;
+
 const fizzbuzzFunctionDemora = (num, demora) => new Promise((resolve, reject) => {
     if (typeof num === 'number'){
         setTimeout(() => {
@@ -36,21 +37,16 @@ const fizzbuzzFunctionDemora = (num, demora) => new Promise((resolve, reject) =>
     }
 })
 
-async function mostrarResultados(ciclos) {
-    const to = (ciclos > 0)? ciclos : 100;
-    // const DEMORA_EN_MS = 0;
+async function mostrarResultados(ciclos = 100) {
 
     try {
-        for (let i = 1; i <= to; i++){
+        for (let i = 1; i <= ciclos; i++){
             const result = await fizzbuzzFunctionDemora(i, randomNumber());
             console.log(result);
-            // setTimeout(() => {
-            //     console.log(result);
-            // }, DEMORA_EN_MS * i);
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }    
-} */
+}
 
 mostrarResultados(ciclos);
